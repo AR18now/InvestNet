35b964f9a54e57940c4a062894db89fffd711ca4
AR18now <128372946+AR18now@users.noreply.github.com>
2024-12-07 22:14:01 +0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
+//import './App.css';
 import AdminDashboard from './components/ui/AdminDashboard';
 import AuthPages from './AuthPages';
 
